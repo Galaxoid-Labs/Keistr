8f586ed7815b31fb701a16087228ab7bfdcee558
Jacob Davis <ismyhc@gmail.com>
2023-02-26 12:07:19 -0500

    Initial Commit

---
+++ Keistr/KeistrApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeistrApp.swift
+//  Keistr
+//
+//  Created by Jacob Davis on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct KeistrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
